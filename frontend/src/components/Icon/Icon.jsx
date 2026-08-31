83a332b71f77ce5bc65153dd027d29050ccbe92a
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2026-08-31 12:11:36 +0200

    fix: change route base to root for direct netlify access

--- frontend/src/components/Icon/Icon.jsx
+++ frontend/src/components/Icon/Icon.jsx
@@ -1,7 +1,7 @@
 export const Icon = ({ id, className, size, height }) => {
   return (
     <svg className={className} height={height} width={size}>
-      <use href={`/personlist/svg/sprite.svg#${id}`}></use>
+      <use href={`/svg/sprite.svg#${id}`}></use>
     </svg>
   );
 };
